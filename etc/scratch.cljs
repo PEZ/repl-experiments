51490981425f56d5d4fe7bd436ea66e1198ef733
Peter Strömberg <pez@pezius.com>
2021-09-22 16:10:36 +0200

    Add a scratch file for tag cloud view

---
+++ etc/scratch.cljs
@@ -0,0 +1,42 @@
+(ns scratch)
+
+(comment
+  [:> rn/SafeAreaView {:style {:flex 1
+                               :justify-content :space-between
+                               :background-color :white}}
+   [:> rn/View {:style {:flex 4
+                        :align-items :stretch}}
+    [:> rn/TextInput {:multiline true
+                      :scroll-enabled true
+                      :style {:flex 1
+                              :margin 10
+                              :padding 5
+                              :font-size 14
+                              :color :blue
+                              :border-width 1}}]]
+   [:> rn/View {:style {:flex 1
+                        :align-self :center
+                        :justify-content :center}}
+    [button {:on-press #()
+             :style {:background-color :blue}}
+     "Work!"]]
+   [:> rn/View {:style {:flex 8}}
+    [:> rn/ScrollView {:style {:flex 1
+                               :padding 10}}
+     (into [:> rn/View {:style {:flex 1
+                                :flex-direction :row
+                                :justify-content :center
+                                :margin 10
+                                :flex-wrap :wrap}}]
+           (map-indexed (fn [i x]
+                          [:> rn/Text {:key i
+                                       :style {:font-size 14
+                                               :padding-horizontal 4
+                                               :padding-vertical 1
+                                               :margin-vertical 3
+                                               :margin-horizontal 2
+                                               :border-width 0.5
+                                               :border-radius 3}} x])
+                        (range 500)))]]
+   [:> StatusBar {:style "auto"}]]
+  )
